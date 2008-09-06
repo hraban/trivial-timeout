66a9e212644d54dc8d8465ea9f073b285849f780
Gary King <gwking@metabang.com>
2008-09-06 17:30:35 -0400

    Remove warning in tests
    darcs-hash:20080906213035-3cc5d-be43cf44bcd217c7b2c473b2fe60c0cdbba685ec.gz

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -18,9 +18,45 @@ these tests are both very unixy
 	      (sleep 1.0)))
 	(error (c)
 	  (declare (ignore c))))
+    (declare (ignore result))
     (ensure (< (first measures) 0.75) :report "timeout worked")
     (ensure (and condition (typep condition 'timeout-error))
 	    :report "Received timeout error")))
 
 
 
+#|
+
+(deffixture remote-triple-store ()
+  ((port 12346))
+  :implements opens-triple-store
+  (:setup
+   (stop-local-server)
+   (start-local-server port)
+   (apply #'create-triple-store "scratch" 
+	  :connection (allegrograph-connection :port port)
+	  :triple-store-class 'remote-triple-store args))
+  (:teardown
+   (close-triple-store)
+   (stop-local-server)))
+
+(deffixture federated-triple-store ()
+  ()
+  :implements opens-triple-store
+  (:setup
+   (create-triple-store "scratch-1")
+   (create-triple-store "scratch-2")
+   (federate-triple-stores "scratch" (list (find-triple-store "scratch-1")
+					   (find-triple-store "scratch-2"))))
+(deffixture basic-triple-store ()
+  ()
+  :implements opens-triple-store
+  (:setup
+   (create-triple-store "scratch")))
+
+
+(deftest foo
+    :requires opens-triple-store
+    )
+
+|#
